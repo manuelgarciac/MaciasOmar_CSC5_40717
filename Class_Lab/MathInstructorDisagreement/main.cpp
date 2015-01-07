973d313cd32685d9f82d716de77c60d5d79ed234
om2154460 <famasa2@gmail.com>
2015-01-07 11:30:16 -0800

    Signed-off-by: om2154460 <famasa2@gmail.com>

---
+++ Class_Lab/MathInstructorDisagreement/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Omar Macias
+ * Created on January 7, 2015, 11:17 AM
+ * Purpose: How easy computers can make mistakes
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Executions begins here!
+int main(int argc, char** argv) {
+    //Declare 3 variables
+    short a,b,c;
+    //Prompt for a and b
+    cout<<"Input 2 short values"<<endl;
+    cin>>a>>b;
+    //Sum the values
+    c=a+b;
+    //Output the results
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+    
+    //Exit stage right
+    
+    return 0;
+}
+
