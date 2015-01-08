8c23e3cbc44619a116b52f9eaed95574b0900886
om2154460 <famasa2@gmail.com>
2015-01-08 08:53:06 -0800

    Signed-off-by: om2154460 <famasa2@gmail.com>

--- Hmwk/Assignment 1/Savitch_9thEd_Chap1_ProgProj5/main.cpp
+++ Hmwk/Assignment 1/Savitch_9thEd_Chap1_ProgProj5/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Omar Macias
  * Created on January 7, 2015, 7:52 PM
- * Purpose: Homework, Program Project
+ * Purpose: Homework, Program Project 5
  */
 
 //System Libraries
@@ -18,7 +18,17 @@ using namespace std;
 //Execution begins here!
 int main(int argc, char** argv) {
     
+    cout<<"   XXXXX"<<endl;
+    cout<<" XX    XX"<<endl;
+    cout<<" X"<<endl;
+    cout<<" X"<<endl;
+    cout<<" X"<<endl;
+    cout<<" X"<<endl;
+    cout<<" X"<<endl;
+    cout<<" XX    XX"<<endl;
+    cout<<"   XXXXX"<<endl;
     
+    //Exit stage right!
     
     return 0;
 }
